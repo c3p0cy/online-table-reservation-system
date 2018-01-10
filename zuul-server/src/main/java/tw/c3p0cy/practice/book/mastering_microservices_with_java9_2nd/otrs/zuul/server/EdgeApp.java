1a291c8943cca2dffc68b7279050b4d52e5378f0
CY-nb <31556438+c3p0cy@users.noreply.github.com>
2018-01-10 18:12:02 +0800

    Set zuul-server is one client of eureka-service

--- zuul-server/src/main/java/tw/c3p0cy/practice/book/mastering_microservices_with_java9_2nd/otrs/zuul/server/EdgeApp.java
+++ zuul-server/src/main/java/tw/c3p0cy/practice/book/mastering_microservices_with_java9_2nd/otrs/zuul/server/EdgeApp.java
@@ -2,11 +2,12 @@ package tw.c3p0cy.practice.book.mastering_microservices_with_java9_2nd.otrs.zuul
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
-// @EnableDiscoveryClient
+@EnableDiscoveryClient
 public class EdgeApp {
 
   public static void main(String[] args) {
