1f6e7212bffa73d6d3988898f118668a3f7a2711
CY-nb <31556438+c3p0cy@users.noreply.github.com>
2018-01-10 18:07:51 +0800

    Impl zuul-server; Mapped by url of service
    Invoke:
    http://localhost:39004/restaurantapi/v1/restaurants?name=o

---
+++ zuul-server/src/main/java/tw/c3p0cy/practice/book/mastering_microservices_with_java9_2nd/otrs/zuul/server/EdgeApp.java
@@ -0,0 +1,16 @@
+package tw.c3p0cy.practice.book.mastering_microservices_with_java9_2nd.otrs.zuul.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+// @EnableDiscoveryClient
+public class EdgeApp {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EdgeApp.class, args);
+  }
+
+}
